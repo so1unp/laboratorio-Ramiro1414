721da080f995c0fbeff9a775a0dda2471a3cf069
Ramiro <ramiro@VSRVIF011.FI>
2024-03-14 18:20:40 -0300

    lab 1 - ejercicio 2

--- lab-01/sum.c
+++ lab-01/sum.c
@@ -4,7 +4,16 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+	
+	int i = 0;
+	int sum = 0;
 
+	for(i=1; i<argc; i++){
+		sum += atoi(argv[i]);
+	}
+	
+	printf("%d\n", sum);
+	
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
