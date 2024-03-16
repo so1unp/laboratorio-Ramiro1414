159cd1caf4a92dd2f2cdb0e4c24de4bd4d348d70
Ramiro <ramiro@VSRVIF011.FI>
2024-03-16 16:45:08 -0300

    laboratorio 1 - ejercicio 4 (histograma horizontal)

--- lab-01/histograma.c
+++ lab-01/histograma.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_WORD_LENGTH 5
+
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+	
+	int c, i, j, k;
+	c = getchar();
+	i = 0;
+	j = 0;
+	k = 0;
+	int letters;
+	letters = 0;
+	int lettersPerWord[MAX_WORD_LENGTH];
+	
+	for (i = 0; i < MAX_WORD_LENGTH; i++) {
+		lettersPerWord[i] = 0;
+	}
+	
+	// Count words length
+	while (c != EOF) {
+	
+	if (c == ' ' || c == '\n') {
+		lettersPerWord[letters-1] = lettersPerWord[letters-1] + 1;
+		letters = 0;	
+	}
+	else {
+		letters++;
+	}
+
+	c = getchar();
+	
+	}	
+
+	printf("\n");
+	
+	// Print histogram
+	for (j = 0; j < MAX_WORD_LENGTH; j++) {
+		printf("%d ", j+1);
+		
+		for (k = 0; k < lettersPerWord[j]; k++) {
+		printf("*");
+		}
+		
+		printf("\n");
+	}
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
