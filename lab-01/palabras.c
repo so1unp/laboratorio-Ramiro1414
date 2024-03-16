beaa467c8188c0b44ec6a92e617cad31d5bc4d50
Ramiro <ramiro@VSRVIF011.FI>
2024-03-16 14:33:18 -0300

    lab 01 - ejercicio 3

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -4,6 +4,22 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+	
+	int c;
+	c = getchar();
+
+	while (1) {
+	
+	if (c == ' ')
+		printf("\n");
+	else
+		putchar(c);	
+
+	c = getchar();
+
+	}	
+
+	printf("\n");
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
