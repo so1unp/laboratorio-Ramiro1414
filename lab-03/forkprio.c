1d25cd33d6bdc49b9bab1bbaeb426e73942456e7
Ramiro <ramiro@VSRVIF011.FI>
2024-04-05 15:39:14 -0300

    ejeercicio 2 segunda version

--- lab-03/forkprio.c
+++ lab-03/forkprio.c
@@ -6,7 +6,13 @@
 #include <sys/resource.h>
 
 void terminateChild(int signum) {
-    printf("hijo terminado por señal: %d", signum);
+    printf("SIGNAL [%d]: hijo con pid: %d y prioridad: %d\n", signum, getpid(), getpriority(PRIO_PROCESS, (id_t) getpid()));
+}
+
+void childLoop(void) {
+    while (1) {
+        // loop infinito que no hace nada
+    }
 }
 
 int busywork(void)
@@ -20,23 +26,26 @@ int busywork(void)
 int main(int argc, char *argv[])
 {
 
-    /* ========================= DESCOMENTAR LUEGO =========================
     // controla que se ingresen solo tres argumentos
-    if (argc != 3) {
-        printf("Numero incorrecto de argumentos (argumentos esperados: 3)");
+    if (argc != 4) {
+        printf("Numero incorrecto de argumentos: %d (argumentos esperados: 3)", argc);
         exit(EXIT_FAILURE);
     }
-    */
+    
+    const int MAX_PRIORITY = 10;
+
+    signal(SIGTERM, terminateChild); // asigna la señal de terminacion al hijo
 
     // argumentos
     int childs = atoi(argv[1]); // cantidad de hijos
     int execSeconds = atoi(argv[2]); // cantidad de segundos de ejecución
-    // int priorityReduction = atoi(argv[3]); // indica si se reducen las prioridades
+    int priorityReduction = atoi(argv[3]); // indica si se reducen las prioridades
 
     int i = 0;
     int j = 0;
     int p = getpid();
     int pids[childs]; // arreglo que almacenara los pids de los procesos hijos
+    int currentPriority = -1; // comienza en -1 porque se incrementa a 0 con el primer fork()
 
     printf("soy el padre con pid: %d\n", p);
 
@@ -46,13 +55,18 @@ int main(int argc, char *argv[])
         if (p > 0) {
             p = fork();
             pids[i-1] = p; // almaceno el pid del nuevo hijo
+            if (priorityReduction == 1) {
+                (currentPriority < MAX_PRIORITY) ? (currentPriority += 1) : (currentPriority = currentPriority); // reduce la prioridad del hijo
+            } else {
+                currentPriority = 0;
+            }
         }
 
         if (p == 0) {
             printf("soy el hijo %d con pid: %d\n", i, getpid());
-            signal(SIGTERM, terminateChild); // asigna la señal de terminacion al hijo
-            busywork(); // pone a ejecutar una tarea al hijo
-            break;
+            nice(currentPriority);
+            childLoop(); // pone a ejecutar una tarea al hijo
+            // break;
         }
         
     }
@@ -61,13 +75,13 @@ int main(int argc, char *argv[])
     if (p > 0) {
 
         sleep( (unsigned int) execSeconds);
-        printf("padre finaliza\n");
 
+        printf("padre finaliza\n");
+        
         for (j = 1; j <= childs; j++) {
-            printf("hijo %d con pid: %d\n", j, pids[j-1]);
             kill(pids[j-1], SIGTERM); // termina la ejecución del hijo
         }
-
+        
     }
 
     exit(EXIT_SUCCESS);
