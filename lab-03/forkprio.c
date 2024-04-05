d4a7efd311801947884045aef2fc7f11a469efa3
Ramiro <ramiro@VSRVIF011.FI>
2024-04-05 02:03:37 -0300

    sig_receiver y primera version ejercicio 2

--- lab-03/forkprio.c
+++ lab-03/forkprio.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/times.h>      // times()
+#include <unistd.h>
+#include <signal.h>
+#include <sys/resource.h>
+
+void terminateChild(int signum) {
+    printf("hijo terminado por señal: %d", signum);
+}
 
 int busywork(void)
 {
@@ -12,5 +19,56 @@ int busywork(void)
 
 int main(int argc, char *argv[])
 {
+
+    /* ========================= DESCOMENTAR LUEGO =========================
+    // controla que se ingresen solo tres argumentos
+    if (argc != 3) {
+        printf("Numero incorrecto de argumentos (argumentos esperados: 3)");
+        exit(EXIT_FAILURE);
+    }
+    */
+
+    // argumentos
+    int childs = atoi(argv[1]); // cantidad de hijos
+    int execSeconds = atoi(argv[2]); // cantidad de segundos de ejecución
+    // int priorityReduction = atoi(argv[3]); // indica si se reducen las prioridades
+
+    int i = 0;
+    int j = 0;
+    int p = getpid();
+    int pids[childs]; // arreglo que almacenara los pids de los procesos hijos
+
+    printf("soy el padre con pid: %d\n", p);
+
+    // se crean los hijos
+    for (i = 1; i <= childs; i++) {
+
+        if (p > 0) {
+            p = fork();
+            pids[i-1] = p; // almaceno el pid del nuevo hijo
+        }
+
+        if (p == 0) {
+            printf("soy el hijo %d con pid: %d\n", i, getpid());
+            signal(SIGTERM, terminateChild); // asigna la señal de terminacion al hijo
+            busywork(); // pone a ejecutar una tarea al hijo
+            break;
+        }
+        
+    }
+
+    // finaliza la ejecución de los hijos
+    if (p > 0) {
+
+        sleep( (unsigned int) execSeconds);
+        printf("padre finaliza\n");
+
+        for (j = 1; j <= childs; j++) {
+            printf("hijo %d con pid: %d\n", j, pids[j-1]);
+            kill(pids[j-1], SIGTERM); // termina la ejecución del hijo
+        }
+
+    }
+
     exit(EXIT_SUCCESS);
 }
--- lab-03/sig_receiver.c
+++ lab-03/sig_receiver.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+void catchSignal(int);
+
+int main(void)
 {
+    printf("pid: %i\n", getpid());
+    
+    int i = 1;
+
+    for(i = 1; i < 32; i++) {
+        signal(i, catchSignal);
+    }
+
+    while( pause() ) {
+
+    }   
+
     exit(EXIT_SUCCESS);
+}
+
+void catchSignal(int signum) {
+    printf("%s\n", strsignal(signum));
 }
