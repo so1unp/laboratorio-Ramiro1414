0831e98e2ca5ac2e7f3fda45518b2fa128ba62c8
Ramiro <ramiro@VSRVIF011.FI>
2024-04-25 18:11:19 -0300

    ejercicio 3 - lab 4

--- lab-04/glob.c
+++ lab-04/glob.c
@@ -5,16 +5,20 @@
 // Variable global que incrementan los hilos.
 static long glob = 0;
 
-void increment_glob(int loops)
+void *increment_glob(void *loops)
 {
+    long numLoops = (long) loops;
+
     int loc, j;
 
     // incrementa glob
-    for (j = 0; j < loops; j++) {
+    for (j = 0; j < numLoops; j++) {
         loc = glob;
         loc++;
         glob = loc;
     }
+
+    pthread_exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char *argv[])
@@ -34,6 +38,16 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Error: ciclos debe ser mayor a cero.\n");
         exit(EXIT_FAILURE);
     }
+
+    // crea hilos
+    pthread_t thread1;
+    pthread_t thread2;
+
+    pthread_create(&thread1, NULL, increment_glob, (void *)(long) loops);
+    pthread_create(&thread2, NULL, increment_glob, (void *)(long) loops);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
     printf("%ld\n", glob);
 
